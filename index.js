9255dacd60d39b4a0a26ce2ccd05bf2b5ff8e6dc
benard <benardboiwo703@gmail.com>
2024-03-29 00:04:00 +0300

    creating and inserting DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code he
+document.querySelector('main#main').remove();
+const newHeader = document.createElement ('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
